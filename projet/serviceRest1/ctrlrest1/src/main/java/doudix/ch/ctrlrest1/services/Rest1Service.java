d3c4df831e15712c8f78387d15445329638689a0
DreamCloud2023 <nilo.perlini@studentfr.ch>
2025-03-31 16:08:27 +0200

    infras-rest1

--- projet/serviceRest1/ctrlrest1/src/main/java/doudix/ch/ctrlrest1/services/Rest1Service.java
+++ projet/serviceRest1/ctrlrest1/src/main/java/doudix/ch/ctrlrest1/services/Rest1Service.java
@@ -1,5 +1,7 @@
 package doudix.ch.ctrlrest1.services;
 
+import java.math.BigInteger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,15 +23,15 @@ public class Rest1Service {
 
     public User login(String username, String password) {
         User user = userRepository.findByUsername(username);
-        //Verification
+        // Verification
         System.out.println("Utilisateur trouvé : " + user);
         System.out.println("Mot de passe en base : " + user.getPassword());
         System.out.println("Mot de passe reçu : " + password);
 
-    if (user != null && user.getPassword().equals(password)) {
-        return user;
-    }
-    return null;
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        return null;
     }
 
     public void logout() {
@@ -37,18 +39,23 @@ public class Rest1Service {
     }
 
     public User addUser(User user) {
-            if (userRepository.findByUsername(user.getUsername()) != null) {
+        if (userRepository.findByUsername(user.getUsername()) != null) {
             throw new RuntimeException("User already exists");
         }
         return userRepository.save(user);
     }
 
-    
-
-    public Post addPost(String content) {
+    public Post addPost(BigInteger creatorId, String title, String description, String imgUrl) {
         Post post = new Post();
-        post.setContent(content);
-        return postRepository.save(post);
+        post.setCreatorId(creatorId);
+        post.setTitle(title);
+        post.setDescription(description);
+        post.setImageUrl(imgUrl);
+
+        Post savedPost = postRepository.save(post);
+        System.out.println("Post enregistré: " + savedPost);
+
+        return savedPost;
     }
 
     public Message addMessage(String content) {
@@ -65,4 +72,3 @@ public class Rest1Service {
         userRepository.deleteById(id);
     }
 }
-
